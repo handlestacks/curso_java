6e658cf11b0298979a6ff6c894f78860dcd05e95
Roger <handlestacks@gmail.com>
2024-12-28 20:19:55 -0300

    Exercicio de fixação

---
+++ secao14/application/Bank.java
@@ -0,0 +1,53 @@
+package secao14.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import secao14.model.entities.Account;
+import secao14.model.exceptions.DomainException;
+
+public class Bank {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = null;
+		
+		try {			
+			sc = new Scanner(System.in);
+			System.out.println("Enter account data:");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+			
+			Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			Double amount = sc.nextDouble();
+			acc.withdraw(amount);
+			
+			System.out.println("New balance: " + acc.getBalance());
+			
+		} catch(DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		} finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
