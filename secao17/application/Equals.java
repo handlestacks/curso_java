22fb70fca6a7cb49a05211c7b379106273a4822c
Roger <handlestacks@gmail.com>
2025-01-05 21:34:38 -0300

    Conhecendo os generics

---
+++ secao17/application/Equals.java
@@ -0,0 +1,15 @@
+package secao17.application;
+
+public class Equals {
+	
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Maria";
+		
+		System.out.println(a.equals(b));
+		System.out.println(a == b);
+		
+	}
+
+}
---
+++ secao17/application/Hashcode.java
@@ -0,0 +1,17 @@
+package secao17.application;
+
+public class Hashcode {
+	
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		String c = "Rogério Brito dos Santos";
+
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		System.out.println(c.hashCode());
+		
+	}
+
+}
